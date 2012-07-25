52b966d06f01e4e2cc80146f2c4d0f59517cab1b
Daniel van Dorp <daniel@vandorp.biz>
2012-07-25 21:05:27 +0200

    better logic

--- default.sh
+++ default.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Refresh
+apt-get update
+apt-get upgrade
+
+# Install my default thingies
+apt-get -y install nano fail2ban screen wget aptitude git
+
 # Cleanup messy OS templates
 service apache2 stop
 service sendmail stop
