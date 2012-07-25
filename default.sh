7b519c8b48968d1aace6ee70457541085db0584e
Daniel van Dorp <daniel@vandorp.biz>
2012-07-25 21:22:47 +0200

    beter removal and cleaning

--- default.sh
+++ default.sh
@@ -12,4 +12,5 @@ service apache2 stop
 service sendmail stop
 service bind9 stop
 service nscd stop
-apt-get purge nscd bind9 sendmail apache2
+service samba stop
+noninteractive apt-get -q -y remove --purge nscd bind9 'apache2*' 'samba*' 'sendmail*'
